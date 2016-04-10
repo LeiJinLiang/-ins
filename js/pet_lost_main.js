d414273da7049163f771a5e6580ed890afdb6f27
LeiJinLiang <466179464@qq.com>
2016-04-11 00:30:42 +0800

    add new file

---
+++ js/pet_lost_main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jinlei on 16/4/10.
+ */
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 2,
+    centeredSlides: true,
+    paginationClickable: true,
+    spaceBetween: 30
+});
